25ba1a7ba1b549cb80446497b8ae8e8f97fc3e6b
user6122\6122 <kyeongseojeon@gmail.com>
2026-08-28 10:07:19 +0900

    OpenAPI 및 application.yml 설정 업데이트

--- back/src/main/java/com/novelty/config/OpenApiConfig.java
+++ back/src/main/java/com/novelty/config/OpenApiConfig.java
@@ -6,15 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+import java.util.List;
+import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     OpenAPI noveltyOpenApi() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server()
+                                .url("https://kyeongseoj-api.dx6project.site")
+                                .description("Production API")
+                ))
                 .info(new Info()
                         .title("Novelty API")
-                        .description("노벨티 프로젝트 REST API")
+                        .description("Novelty 프로젝트 REST API")
                         .version("v1"));
     }
 }
