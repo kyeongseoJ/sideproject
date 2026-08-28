7b8793b7294dd726e8cf9ee923bbb84e9aa96538
user6122\6122 <kyeongseojeon@gmail.com>
2026-08-28 10:15:06 +0900

    UserMissionController.java에서 GetMapping 임포트 추가

--- back/src/main/java/com/novelty/mission/UserMissionController.java
+++ back/src/main/java/com/novelty/mission/UserMissionController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/user-missions")
